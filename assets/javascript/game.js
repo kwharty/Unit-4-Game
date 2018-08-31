b872dc987a1a2ad2bab13e8c74afdcffac6f5f69
Jay Wright <kwharty@gmail.com>
2018-08-30 21:27:28 -0700

    initial working files

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+//initialize document
+
+//NOT WORKING
+//$(document).ready(function() { 
+
+
+// declare vars
+
+var jalenVal = 0;
+var donteVal = 0;
+var mikalVal = 0;
+var omariVal = 0;
+
+var theirScore = theirScore=Math.floor(Math.random()*101)+19;
+var yourScore = 0;
+var wins = 0;
+var losses = 0;
+
+//set "their score" opponents score to TIE //added direct to variable above
+
+function initialize() {
+    //theirScore=Math.floor(Math.random()*101)+19;
+
+
+
+
+};
+
+//check
+console.log("THEIR SCORE " + "=" + theirScore);
+
+
+//TODO: NOT WORKING BECAUSE DOCUMENT .READY FUNCTION IMPROPER??? UGHH
+// });
